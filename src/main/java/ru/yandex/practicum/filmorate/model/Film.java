458e4804c46b900b6c6d4e93c0dd5c957ac4052b
Владислав <v.konykhov@yandex.ru>
2022-10-23 01:51:55 +0300

    Чистый лист

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,26 +2,14 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
-import javax.validation.constraints.*;
+import java.time.Duration;
 import java.time.LocalDate;
-
 
 @Data
 public class Film {
-
-    private Integer id;
-    @NotBlank
-    private  String name;
-    @NotBlank
-    @Size(min=0,max = 200)
-    private  String description;
-    @NotNull
-    private final LocalDate releaseDate;
-    @Min(1)
-    @Positive
-    private  int duration;
-
-    public Film(LocalDate releaseDate) {
-        this.releaseDate = releaseDate;
-    }
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,22 +2,13 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 @Data
 public class User {
-    private Integer id;
-    @NotNull
+    private int id;
+    private String email;
+    private String login;
     private String name;
-    @NotBlank
-    @Email
-    private final String email;
-    @NotBlank
-    private final String login;
-    @PastOrPresent
-    private final LocalDate birthday;
+    private LocalDate birthday;
 }
