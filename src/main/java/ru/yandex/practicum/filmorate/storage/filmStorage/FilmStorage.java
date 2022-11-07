1abe0cf34698c833359384306473f5795fc1567a
Владислав <v.konykhov@yandex.ru>
2022-11-07 22:53:48 +0300

    Создал основные пакеты и классы/интерфейсы

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/filmStorage/FilmStorage.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.filmorate.storage.filmStorage;
+
+public interface FilmStorage {
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/filmStorage/InMemoryFilmStorage.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.filmorate.storage.filmStorage;
+
+public class InMemoryFilmStorage {
+}
