332ff7183346dc4f6561a088e38b8a64b3e54a2d
Владислав <v.konykhov@yandex.ru>
2023-01-17 23:28:14 +0300

    подборка добавления тестов

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.service.GenreService;
+import ru.yandex.practicum.filmorate.service.GenreService;
+
 
 import java.util.List;
 @RequiredArgsConstructor
